4e7c01d290ca6971580624dd0f205f7a31891964
Victor Sobol <vsobol@maven.co>
2022-08-22 13:05:17 +0300

    Use valid symver version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name="pysolr",
-    version="3.3.3",
+    version="3.3.3.dev0",
     description="Lightweight python wrapper for Apache Solr.",
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
